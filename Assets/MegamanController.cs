76f2e85448a92996660df75f8c7d261e97070b65
Porfirio Partida MAC <porfirio.partidai@gmail.com>
2016-02-15 13:35:55 -0700

    Megaman Idle <-> Running Kreygasm

---
+++ Assets/MegamanController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MegamanController : MonoBehaviour {
+	public Animator MyAnimator;
+	// Use this for initialization
+	void Start () {
+		MyAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool isHorizontal = Input.GetAxis ("Horizontal") > 0.2;
+		MyAnimator.SetBool("isRunning", isHorizontal);
+	}
+}
