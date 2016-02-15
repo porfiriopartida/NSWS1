c2ed12c8fdf7f784c95e4a01fd773fef2435b8a1
Porfirio Partida MAC <porfirio.partidai@gmail.com>
2016-02-15 13:55:29 -0700

    Flip + Fix Sprite

--- Assets/MegamanController.cs
+++ Assets/MegamanController.cs
@@ -7,10 +7,19 @@ public class MegamanController : MonoBehaviour {
 	void Start () {
 		MyAnimator = GetComponent<Animator>();
 	}
-	
+	bool facingRight = true;
 	// Update is called once per frame
 	void Update () {
-		bool isHorizontal = Input.GetAxis ("Horizontal") > 0.2;
+		float hVal = Input.GetAxis ("Horizontal");
+		bool isHorizontal = hVal != 0;
+
+		if (hVal > 0 && !facingRight || hVal < 0 && facingRight){
+			switchSide();
+		}
 		MyAnimator.SetBool("isRunning", isHorizontal);
+	}
+	void switchSide(){
+		facingRight = !facingRight;
+		transform.localScale = new Vector3(transform.localScale.x * -1, 1, 1);
 	}
 }
