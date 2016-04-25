0f20fa7c3fabcb89d9730bbb34a378f7ca099ff8
Porfirio Partida - home <porfirio.partidai@gmail.com>
2016-04-25 00:22:15 -0700

    add on trigger enter for megaman controller and check for collision with enemy

--- Assets/MegamanController.cs
+++ Assets/MegamanController.cs
@@ -28,6 +28,8 @@ public class MegamanController : MonoBehaviour {
 	}
 	bool facingRight = true;
 	bool isGrounded = true;
+	bool canMove = true;
+	float nextCanMove = 0;
 	// Update is called once per frame
 	void Update () {
 		//float hVal = Input.GetAxis ("Horizontal");
@@ -48,20 +50,20 @@ public class MegamanController : MonoBehaviour {
 		}
 		isHorizontal = hVal != 0;
 
-		if(hVal != 0){
+		if(canMove && hVal != 0){
 			MyRigidbody.velocity = new Vector2(hVal * MovementSpeed, MyRigidbody.velocity.y);
 			if (hVal > 0 && !facingRight || hVal < 0 && facingRight){
 				switchSide();
 			}
 		}
 
-		if(isGrounded && isJump){
+		if(canMove && isGrounded && isJump){
 			jumpAudio.Play ();
 			MyRigidbody.AddForce(new Vector2(0, JumpForce));
 			isGrounded = false;
 		}
 		isGrounded = Physics2D.OverlapCircle (GroundCheck.position, groundCheckRadius, WhatIsGround);
-		
+
 		if(healthBar != null){
 			healthBar.value = currentHP;
 		}
@@ -73,13 +75,52 @@ public class MegamanController : MonoBehaviour {
 		MyAnimator.SetBool("isRunning", isHorizontal);
 		MyAnimator.SetBool("isShooting", isShooting);
 		MyAnimator.SetBool("isDashing", isDashing);
+
+		if (!canMove) {
+			nextCanMove -= Time.deltaTime;
+			if(nextCanMove <= 0){
+				canMove = true;
+			}
+		}
 	}
 	
-	//void OnTriggerEnter2D(Collider2D other)
-	//{
-		//print (other.transform.ToString());
-		//this.score++;
-	//}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Transform transform = other.transform;
+		GameObject obj = transform.gameObject;
+		if(obj){
+			string tag = obj.tag;
+			if(tag.Equals("NPC_ENEMY")){
+				addDamage(10);
+				float xSpeed = JumpForce;
+				float ySpeed = JumpForce;
+				int xDirection = transform.position.x - this.transform.position.x > 0 ? -1:1;
+				int yDirection = transform.position.y - this.transform.position.y > 0 ? -1:1;
+				print ("xDirection: " + xDirection);
+				print ("yDirection: " + yDirection);
+				print ("transform.position.x - this.transform.position.x : " + (transform.position.x - this.transform.position.x) );
+				print ("transform.position.y - this.transform.position.y: " + (transform.position.y - this.transform.position.y));
+				MyRigidbody.AddForce(new Vector2(xSpeed * xDirection, ySpeed * yDirection));
+				this.canMove = false;
+				nextCanMove = 1.5f;
+				//playerController.addScore (1);
+				//Destroy(obj);
+			} else if(tag.Equals("ENEMY_BULLET")){
+				addDamage(10);
+				Destroy(obj);
+			}
+		}
+		
+	}
+	public void addDamage(int dmg){
+		this.currentHP -= dmg;
+		if(currentHP < 0){
+			currentHP = 0;
+			Destroy(this.gameObject);
+			//die.
+		}
+	}
+
 	public void addScore(int v){
 		this.score += 1;
 	}
