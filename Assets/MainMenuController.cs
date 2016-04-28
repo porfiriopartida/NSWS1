97b3e95efe96a169b365a81799872b1d7a0b6911
Porfirio Partida - home <porfirio.partidai@gmail.com>
2016-04-28 00:00:25 -0700

    Generic main menu and prepare build

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+	bool over;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool clicked = Input.GetMouseButtonDown (0);
+		if (clicked) {
+			print ("Clicked: ");
+		}
+		if (clicked && over)
+		{
+			startGame();
+		}
+	}
+	
+	
+	private void startGame()
+	{
+		//Reset score
+		PlayerPrefs.SetInt("score", 0);
+		PlayerPrefs.SetInt("Current", 1);
+		PlayerPrefs.SetInt("currentHP", 100);
+		//		Application.LoadLevel("intro");
+		//Loads intro, or lv 1 for now.
+		Application.LoadLevel("Level1");
+	}
+
+	void OnMouseEnter()
+	{
+		print ("Mouse enter");
+		over = true;
+	}
+	
+	void OnMouseExit()
+	{
+		over = false;
+	}
+}
